ffb26c34056373db2fd626e793ef818d629a8061
Vasilena Voynikova <38500944+vvoynikova@users.noreply.github.com>
2018-07-11 16:12:00 +0200

    Changes made

--- config/config_deploy.js
+++ config/config_deploy.js
@@ -1,23 +1,19 @@
 var config_deploy = {
-    
+
     // obligatory fields
-	
+
     // the experimentID is needed to recover data from the _babe server app
 	// you receive the experimentID when you create the experiment using the _babe server app
-	
-	 "experimentID": "8",
+
+	 "experimentID": "6",
 
     // set deployment method; use one of:
-    //'debug', 'localServer', 'MTurk', 
+    //'debug', 'localServer', 'MTurk',
     // 'MTurkSandbox', 'Prolific', 'directLink'
-    "deployMethod" : "directLink", 
-    
+    "deployMethod" : "directLink",
+
     // optional fields
-    
+
     // who to contact in case of trouble
-    "contact_email": "YOUREMAIL@wherelifeisgreat.you", 
+    "contact_email": "YOUREMAIL@wherelifeisgreat.you",
 };
-
-
-
-   
